36d5f6c1a79f68863551f50ac4b5d50cd2839ce2
neeraj-3110 <neeraj4630@gmail.com>
2026-03-23 23:01:24 +0530

    Delete Desktop/campus-lost-found directory

--- Desktop/campus-lost-found/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = 'secret123'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///campus.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'app/static/uploads'
